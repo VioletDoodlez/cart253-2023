d7b55bed589e985c136b7b01488a3766d64247f9
VioletDoodlez <nicolecovaliu@gmail.com>
2023-09-19 15:06:26 -0400

    Added colour and changed background

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * I Like To Move It
+ * Nicole Covaliu
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -16,33 +16,45 @@ function preload() {
 }
 
 let bg = {
-    r: 240,
-    g: 240,
-    b: 240
+    r: 0,
+    g: 0,
+    b: 0
 }
 
 let circle1 = {
     x: 250,
     y: 0,
     size: 50,
-    fill: 255,
-    alpha: 200
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0,
+    },
+    alpha: 140
 }
 
 let circle2 = {
     x: 0,
     y: 500,
     size: 50,
-    fill: 255,
-    alpha: 200
+    fill: {
+        r: 255,
+        g: 255,
+        b: 0,
+    },
+    alpha: 160
 }
 
 let circle3 = {
     x: 500,
     y: 500,
     size: 50,
-    fill: 255,
-    alpha: 200
+    fill: {
+        r: 0,
+        g: 0,
+        b: 255,
+    },
+    alpha: 150
 }
 
 /**
@@ -66,8 +78,8 @@ function draw() {
     circle1.y = constrain(circle1.y, 0, 210);
     circle1.size = circle1.size + 0.25;
     circle1.size = constrain(circle1.size, 0, 150);
-    circle1.fill = map(mouseX,0,width,0,255);
-    fill(circle1.fill, circle1.alpha);
+    circle1.fill.b = map(mouseX,0,width,0,255);
+    fill(circle1.fill.r, circle1.fill.g, circle1.fill.b, circle1.alpha);
     ellipse(circle1.x, circle1.y, circle1.size);
     
     //Circle 2
@@ -77,8 +89,8 @@ function draw() {
     circle2.y = constrain(circle2.y, 300, 500);
     circle2.size = circle2.size + 0.25;
     circle2.size = constrain(circle2.size, 0, 150);
-    circle2.fill = map(mouseY,0,width,0,255);
-    fill(circle2.fill, circle2.alpha);
+    circle2.fill.r = map(mouseY,0,width,0,255);
+    fill(circle2.fill.r, circle2.fill.g, circle2.fill.b, circle2.alpha);
     ellipse(circle2.x, circle2.y, circle2.size);
 
     //Circle 3
@@ -88,8 +100,8 @@ function draw() {
     circle3.y = constrain(circle3.y, 300, 500);
     circle3.size = circle3.size + 0.25;
     circle3.size = constrain(circle3.size, 0, 150);
-    circle3.fill = map(mouseX+mouseY,0,width,0,255);
-    fill(circle3.fill, circle3.alpha);
+    circle3.fill.g = map(mouseX+mouseY,0,width,0,255);
+    fill(circle3.fill.r, circle3.fill.g, circle3.fill.b, circle3.alpha);
     ellipse(circle3.x, circle3.y, circle3.size);
 
 }  
