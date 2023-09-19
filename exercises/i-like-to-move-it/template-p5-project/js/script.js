4c15a5ea9663146539d0205338b6581c1f4091c6
VioletDoodlez <nicolecovaliu@gmail.com>
2023-09-18 22:57:24 -0400

    Update script.js
    Added shapes and movement

--- exercises/i-like-to-move-it/template-p5-project/js/script.js
+++ exercises/i-like-to-move-it/template-p5-project/js/script.js
@@ -15,12 +15,42 @@ function preload() {
 
 }
 
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+}
+
+let circle1 = {
+    x: 250,
+    y: 0,
+    size: 50,
+    fill: 255,
+    alpha: 200
+}
+
+let circle2 = {
+    x: 0,
+    y: 500,
+    size: 50,
+    fill: 255,
+    alpha: 200
+}
+
+let circle3 = {
+    x: 500,
+    y: 500,
+    size: 50,
+    fill: 255,
+    alpha: 200
+}
 
 /**
  * Description of setup
 */
 function setup() {
     createCanvas(500,500);
+    noStroke();
 }
 
 
@@ -28,7 +58,35 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(255,0,0);
-    rectMode(CENTER);
-    rect(250,250,100,100);
-}
+    //Background
+    background(bg.r,bg.g,bg.b);
+    
+    //Circle 1
+    circle1.y = circle1.y + 1;
+    circle1.y = constrain(circle1.y, 0, 210);
+    circle1.size = circle1.size + 0.25;
+    circle1.size = constrain(circle1.size, 0, 150);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+    
+    //Circle 2
+    circle2.x = circle2.x + 1;
+    circle2.y = circle2.y - 1;
+    circle2.x = constrain(circle2.x, 0, 200);
+    circle2.y = constrain(circle2.y, 300, 500);
+    circle2.size = circle2.size + 0.25;
+    circle2.size = constrain(circle2.size, 0, 150);
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
+
+    //Circle 3
+    circle3.x = circle3.x - 1;
+    circle3.y = circle3.y - 1;
+    circle3.x = constrain(circle3.x, 300, 500);
+    circle3.y = constrain(circle3.y, 300, 500);
+    circle3.size = circle3.size + 0.25;
+    circle3.size = constrain(circle3.size, 0, 150);
+    fill(circle3.fill, circle3.alpha);
+    ellipse(circle3.x, circle3.y, circle3.size);
+
+}  
