4721dbc18bda8258cf5dd900055e20b72cd919b9
VioletDoodlez <nicolecovaliu@gmail.com>
2023-10-11 16:22:04 -0400

    Added caterpillar

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -8,7 +8,7 @@
 
 "use strict";
 
-let circle1 = {
+/*let user = {
     x: 250,
     y: 250,
     size: 150,
@@ -16,6 +16,14 @@ let circle1 = {
     vx: 0,
     vy: 0,
     speed: 2
+}*/
+
+let caterpillar = {
+    x: 100,
+    y: 250,
+    totalSegments: 10,
+    segmentSize: 100,
+    segmentSpacing: 70,
 }
 
 
@@ -32,7 +40,7 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
 }
 
 
@@ -40,11 +48,79 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background();
-    display();
+    background(0);
+
+    let x = caterpillar.x;
+
+    let segmentsDrawn = 0;
+
+    while (segmentsDrawn < caterpillar.totalSegments) {
+        noStroke();
+        fill(100, 200, 100);
+        ellipse(x, caterpillar.y, caterpillar.segmentSize);
+
+        x = x + caterpillar.segmentSpacing;
+
+        segmentsDrawn = segmentsDrawn + 1;
+    }
+    //move ();
+    //display();
+}
+
+
+
+/*function move() {
+    // user mouvement
+    if (keyIsDown (LEFT_ARROW)) {
+        user.vx = -user.speed;
+    }
+    else if (keyIsDown (RIGHT_ARROW)) {
+        user.vx = user.speed;
+    }
+    else {
+        user.vx = 0;
+    }
+
+    if (keyIsDown (UP_ARROW)) {
+        user.vy = -user.speed;
+    }
+    else if (keyIsDown (DOWN_ARROW)) {
+        user.vy = user.speed;
+    }
+    else {
+        user.vy = 0;
+    }
+
+    user.x = user.x + user.vx;
+    user.y = user.y + user.vy;
+
+    // caterpillar mouvement
+    if (keyIsDown (LEFT_ARROW)) {
+        user.vx = -user.speed;
+    }
+    else if (keyIsDown (RIGHT_ARROW)) {
+        user.vx = user.speed;
+    }
+    else {
+        user.vx = 0;
+    }
+
+    if (keyIsDown (UP_ARROW)) {
+        user.vy = -user.speed;
+    }
+    else if (keyIsDown (DOWN_ARROW)) {
+        user.vy = user.speed;
+    }
+    else {
+        user.vy = 0;
+    }
+
+    user.x = user.x + user.vx;
+    user.y = user.y + user.vy;
 }
 
 function display() {
-    fill(circle1.fill);
-    ellipse(circle1.x, circle1.y, circle1.size);
-}
+    //displays user
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+}*/
