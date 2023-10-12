8acac7ff5c5cc21c328a2aea1e129053d938b9a5
VioletDoodlez <nicolecovaliu@gmail.com>
2023-10-12 14:07:36 -0400

    Changed caterpillar to spider, added simulation function

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -18,17 +18,14 @@ let bug = {
     speed: 2
 }
 
-let caterpillar = {
+let spider = {
     x: 250,
-    y: 250,
-    totalSegments: 10,
-    segmentSize: 100,
-    segmentSpacing: 70,
+    y: 0,
+    size: 100,
     vx: 0,
     vy: 0,
-    segmentSpeed: 3
+    speed: 5
 }
-
 
 
 /**
@@ -44,6 +41,9 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
+
+    bug.vx = random(-bug.speed, bug.speed);
+    bug.vy = random(-bug.speed, bug.speed);
 }
 
 
@@ -53,74 +53,64 @@ function setup() {
 function draw() {
     background(0);
 
-    let x = caterpillar.x;
-
-    let segmentsDrawn = 0;
-
-    while (segmentsDrawn < caterpillar.totalSegments) {
-        noStroke();
-        fill(100, 200, 100);
-        ellipse(x, caterpillar.y, caterpillar.segmentSize);
-
-        x = x + caterpillar.segmentSpacing;
-
-        segmentsDrawn = segmentsDrawn + 1;
-    }
-    move ();
-    display();
+     move();
+     display();
 }
 
 
 
 function move() {
     // bug mouvement
-    let dx = bug.x - caterpillar.x;
-    let dy = bug.y - caterpillar.y;
-
-    if (dx < 0) {
-        bug.vx = -bug.speed;
-    }
-    else if (dx > 0) {
-        bug.vx = bug.speed;
-    }
-
-    if (dy < 0) {
-        bug.vy = -bug.speed;
-    }
-    else if (dy > 0) {
-        bug.vy = bug.speed;
-    }
-
-    bug.x = bug.x + bug.vx;
-    bug.y = bug.y + bug.vy;
-
-    // caterpillar mouvement
-    if (keyIsDown (LEFT_ARROW)) {
-        caterpillar.vx = -caterpillar.segmentSpeed;
-    }
-    else if (keyIsDown (RIGHT_ARROW)) {
-        caterpillar.vx = caterpillar.segmentSpeed;
-    }
-    else {
-        caterpillar.vx = 0;
-    }
-
-    if (keyIsDown (UP_ARROW)) {
-        caterpillar.vy = -caterpillar.segmentSpeed;
-    }
-    else if (keyIsDown (DOWN_ARROW)) {
-        caterpillar.vy = caterpillar.segmentSpeed;
-    }
-    else {
-        caterpillar.vy = 0;
-    }
-
-    caterpillar.x = caterpillar.x + caterpillar.vx;
-    caterpillar.y = caterpillar.y + caterpillar.vy;
+     let dx = bug.x - spider.x;
+     let dy = bug.y - spider.y;
+ 
+     if (dx < 0) {
+         bug.vx = -bug.speed;
+     }
+     else if (dx > 0) {
+         bug.vx = bug.speed;
+     }
+ 
+     if (dy < 0) {
+         bug.vy = -bug.speed;
+     }
+     else if (dy > 0) {
+         bug.vy = bug.speed;
+     }
+ 
+     bug.x = bug.x + bug.vx;
+     bug.y = bug.y + bug.vy;
+ 
+     // spider mouvement
+     if (keyIsDown (LEFT_ARROW)) {
+         spider.vx = -spider.speed;
+     }
+     else if (keyIsDown (RIGHT_ARROW)) {
+         spider.vx = spider.speed;
+     }
+     else {
+         spider.vx = 0;
+     }
+ 
+     if (keyIsDown (UP_ARROW)) {
+         spider.vy = -spider.speed;
+     }
+     else if (keyIsDown (DOWN_ARROW)) {
+         spider.vy = spider.speed;
+     }
+     else {
+         spider.vy = 0;
+     }
+ 
+     spider.x = spider.x + spider.vx;
+     spider.y = spider.y + spider.vy;    
 }
 
 function display() {
-    //displays bug
-    fill(bug.fill);
-    ellipse(bug.x, bug.y, bug.size);
+     //displays spider
+     fill(100, 200, 100);
+     ellipse(spider.x, spider.y, spider.size);
+     //displays bug
+     fill(bug.fill);
+     ellipse(bug.x, bug.y, bug.size);
 }
